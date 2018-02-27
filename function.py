86643107ac9dcbbcbd59b786c8a8387949a3ba4d
ViktorTestov <mykyteektest1@gmail.com>
2018-02-27 23:09:54 +0200

    MINOR:Add new files

---
+++ function.py
@@ -0,0 +1,39 @@
+# -*- coding utf-8 -*-
+
+#приклад змінних
+print "Variables Demo"
+
+#задаємо значення для змінної 1
+v = 1
+print "v = ", v
+#додаємо 2 до попереднього значення
+v = "v + 2", v
+# присвоюєм нове значення 
+
+v = 52
+print "v = ", v
+
+#множимо на 5
+print "v * 5 =", v*5
+#значення змінної не змінилось
+print "v = ", v*5
+
+#щоб значення змінилось потрібно напряму задати значення
+v = v * 5
+print "v = v * 5"
+print "v = ", v
+
+
+word1 = "Good"
+word2 = "Morning"
+word3 = "to tou too"
+
+print word1, word2
+
+sentence = word1 + " "+ word2 + " " + word3
+
+print sentence
+
+p = 1
+print "p +2", p
+
